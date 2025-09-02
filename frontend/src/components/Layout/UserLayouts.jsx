710358da71f2ac262eb0774fa94c7640ddc4e8e4
KeyCode <agyeis6000@gmail.com>
2025-09-02 03:30:10 -0700

    basics of project

---
+++ frontend/src/components/Layout/UserLayouts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../common/Header'
+
+const UserLayouts = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default UserLayouts
---
+++ frontend/src/components/common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../Layout/Topbar'
+
+const Header = () => {
+  return (
+    <>
+    {/* ?Topbar */}
+    <Topbar />
+    {/* navbar */}
+    {/* cart drawer */}
+    </>
+  )
+}
+
+export default Header
